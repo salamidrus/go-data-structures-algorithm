7750f4051c0bf0d1825526b2ed8c1cfd6a6e713a
salamidrus <47554255+salamidrus@users.noreply.github.com>
2023-10-26 09:10:11 +0700

    feat(frequency counter): same refactored function

--- problem solving pattern/frequency counter/main.go
+++ problem solving pattern/frequency counter/main.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
-	fmt.Println(sameNaive([]int{1, 2, 3}, []int{4, 1, 9}))
+	fmt.Println(sameNaive([]int{1, 2, 3}, []int{5, 1, 9}))      // should return false
+	fmt.Println(sameNaive([]int{1, 2, 3}, []int{4, 1, 9}))      // should return true
+	fmt.Println(sameRefactored([]int{1, 2, 3}, []int{5, 1, 9})) // should return false
+	fmt.Println(sameRefactored([]int{1, 2, 3}, []int{4, 1, 9})) // should return true
 }
 
 func sameNaive(arr1 []int, arr2 []int) bool {
@@ -41,4 +44,42 @@ func checkValueByIndex(slice []int, val int) int {
 	}
 
 	return -1
+}
+
+func sameRefactored(arr1 []int, arr2 []int) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+
+	var (
+		frequencyCounter1 = make(map[int]int)
+		frequencyCounter2 = make(map[int]int)
+	)
+
+	for _, v := range arr1 {
+		_, ok := frequencyCounter1[v]
+		if ok {
+			frequencyCounter1[v] = frequencyCounter1[v] + 1
+		} else {
+			frequencyCounter1[v] = 1
+		}
+	}
+
+	for _, v := range arr2 {
+		_, ok := frequencyCounter2[v]
+		if ok {
+			frequencyCounter2[v] = frequencyCounter2[v] + 1
+		} else {
+			frequencyCounter2[v] = 1
+		}
+	}
+
+	for k, v := range frequencyCounter1 {
+		counter, ok := frequencyCounter2[int(math.Pow(float64(k), 2))]
+		if !ok || counter != v {
+			return false
+		}
+	}
+
+	return true
 }
